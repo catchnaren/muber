e7b98743a83a4ad8684fced82ccf3ca96b1ecc26
catchnaren <narenckumar@gmail.com>
2022-07-24 19:13:21 +0530

    Implement locating cab drivers service

---
+++ README.md
@@ -0,0 +1,3 @@
+## Backend App for finding cab drivers
+
+A node/express backend implementation with mocha test-driven approach for locating nearest cab drivers like uber with MongoDB and GeoJSON for encoding geographic data structures.
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const app = express();
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/muber');
+}
+
+app.use(express.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
+
+module.exports = app;
